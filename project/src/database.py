a16dab52a08772dd2c4f00b0600cefbdedf7f011
sadnastya@github.com <sadnastya@github.com>
2025-02-04 17:37:11 +0300

    4-5-6 tasks

--- project/src/database.py
+++ project/src/database.py
@@ -1,19 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 url = URL.create(
     drivername="postgresql",
-    username="postgres",
-    password="password",
-    host="db",
-    port=5432,
+    username=os.getenv("USERNAME_POSTGRESQL"),
+    password=os.getenv("PASSWORD_POSTGRESQL"),
+    host=os.getenv("HOST_POSTGRESQL"),
+    port=os.getenv("PORT_POSTGRESQL"),
     database="app"
 )
 
 engine = create_engine(url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
